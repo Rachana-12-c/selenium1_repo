f34ca481ad2b3780a41c0f6794e6075e9f921ebe
Rachana-12-c <rachanac270898@gmail.com>
2024-11-16 12:55:41 +0530

    Update ToLearnGetPageSource.java

--- Automation/src/WebDriver/ToLearnGetPageSource.java
+++ Automation/src/WebDriver/ToLearnGetPageSource.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ToLearnGetPageSource {
-
+//This class is modified
 	public static void main(String[] args) {
 		WebDriver driver=new ChromeDriver();
 		
